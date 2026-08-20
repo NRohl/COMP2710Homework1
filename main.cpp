769ad6122d2bcb976def137048ee230e2102d446
Nathaniel Rohling <ner0024@auburn.edu>
2026-08-20 14:00:55 -0500

    For assignment

--- main.cpp
+++ main.cpp
@@ -8,4 +8,7 @@ int main()
 	//create a few starter objects
 	//add some values to test out your values methods
 	//print out all the values for each object to validate your code
+	printf("Text Output is as follows: ");
+	char data[] = "This is the first homework assignment. 8/20/2026";
+	printf("%s\n", data);
 }
